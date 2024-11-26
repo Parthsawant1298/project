e18a1c8036fa738f44ff4ef8a47acc23aebd2469
Parthsawant1298 <145202363+Parthsawant1298@users.noreply.github.com>
2024-11-27 00:36:56 +0530

    Update Benefits.jsx

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,6 +1,6 @@
 import { benefits } from "../constants";
 import Heading from "./Heading";
-import Section from "./section";
+import Section from "./Section";
 import Arrow from "../assets/svg/Arrow";
 import { GradientLight } from "./design/Benefits";
 import ClipPath from "../assets/svg/ClipPath";
